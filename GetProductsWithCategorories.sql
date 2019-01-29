a0093e72d06a9f9964620fb1419532fe82cad756
skynet20beta <skynet20beta@users.noreply.github.com>
2019-01-29 15:09:58 +0300

    Delete GetProductsWithCategorories.sql

--- GetProductsWithCategorories.sql
+++
@@ -1,5 +0,0 @@
-use Catalogue;
-
-select p.Name as 'Product name', coalesce(c.Name, '') as 'Category name' from Product (nolock) p
-left join ProductToCategory (nolock) pc on p.ID = pc.ProductId
-left join Category (nolock) c on c.ID = pc.CategoryId
